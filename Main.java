8d91726916addf596ea54b42e2bcef380ae9b244
gravityguy6555-g26 <129086823+gravityguy6555@users.noreply.github.com>
2023-09-27 10:28:58 -0400

    MES QUE UN CLUB

--- Main.java
+++ Main.java
@@ -1,3 +1,10 @@
 public class Main {
-    
+    public static void main(String[] args) {
+        LaMasiaTalent b1 = new LaMasiaTalent();
+        LaMasiaTalent b2 = new LaMasiaTalent();
+        LaMasiaTalent b3 = new LaMasiaTalent();
+
+        laMasiaTalent.type("LaMasiaTalent for Barcelona")
+        laMasiaTalent.
+    }
 }
