a9ad934c5064a332b0c97f5ee19c61dc82137095
gravityguy6555-g26 <129086823+gravityguy6555@users.noreply.github.com>
2023-10-05 10:26:56 -0400

    ez update

--- La Masia.java
+++ La Masia.java
@@ -1,3 +1,15 @@
 public class La Masia {
-    
+
+    String nationality = "None";
+    String position = "None";
+    String name = "None";
+    String foot = "None";
+    String strength = "None";
+    int debut_date = "0";
+    String status = "None";
+
+
+public void whatCountry(String nationality){
+    this.nationality = nationality;
+}
 }
--- Main.java
+++ Main.java
@@ -3,8 +3,66 @@ public class Main {
         LaMasiaTalent b1 = new LaMasiaTalent();
         LaMasiaTalent b2 = new LaMasiaTalent();
         LaMasiaTalent b3 = new LaMasiaTalent();
+        LaMasiaTalent b4 = new LaMasiaTalent();
+        LaMasiaTalent b5 = new LaMasiaTalent();
+        LaMasiaTalent b6 = new LaMasiaTalent();
+        LaMasiaTalent b7 = new LaMasiaTalent();
+       
+b1.nationality("Spain");
+b1.position("Central Midfielder");
+b1.name("Xavi Hernandez");
+b1.foot("Right footed");
+b1.player_strength("Passing");
+b1.debut_date("10/03/98");
+b1.status("GOAT midfielder");
 
-        laMasiaTalent.type("LaMasiaTalent for Barcelona")
-        laMasiaTalent.
+b2.nationality("Argentina");
+b2.position("Right Wing");
+b2.name("Lionel Messi");
+b2.foot("Left footed");
+b2.player_strength("Everything");
+b2.debut_date("10/16/04");
+b2.status("GOAT");
+
+b3.nationality("Spain/Morocco");
+b3.position("Right Wing");
+b3.name("Lamine Yamal");
+b3.foot("Left footed");
+b3.player_strength("Everything");
+b3.debut_date("04/29/23");
+b3.status("Future GOAT");
+
+b4.nationality("Spain");
+b4.position("Central Midfielder");
+b4.name("Pablo Gavi");
+b4.foot("Right footed");
+b4.player_strength("Work ethic");
+b4.debut_date("08/29/21");
+b4.status("Future Goat Midfielder");
+
+b5.nationality("Spain");
+b5.position("Right Back");
+b5.name("Alejandro Balde");
+b5.foot("Left footed");
+b5.player_strength("SPEED");
+b5.debut_date("09/12/21");
+b5.status("Future Legend");
+
+b6.nationality("Spain");
+b6.position("Central Midfielder");
+b6.name("Andres Iniesta");
+b6.foot("Right footed");
+b6.player_strength("Dribbling and passing");
+b6.debut_date("12/21/02");
+b6.status("Legendary midfielder");
+
+b7.nationality("Spain");
+b7.position("Central Defensive Midfielder");
+b7.name("Sergio Busquets ");
+b7.foot("Right footed");
+b7.player_strength("Passing and intelligence");
+b7.debut_date("10/22/08");
+b7.status("GOAT Defensive Midfielder");
+
     }
 }
